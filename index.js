084725fcb6bd15dfe0c2678e7b6f58595d2d664d
namneeeet <139070475+namneeet@users.noreply.github.com>
2024-06-14 16:09:38 -0300

    added scores and visuals

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let lastPaintTime = 0;
 let snakeArr = [{x:13, y:15}];
 let food = {x:6, y:7};
 let score = 0;
+let time = 0;
+let highscore = 0;
 
 
 //Game Functions
@@ -21,23 +23,59 @@ function main(ctime){
 }
 
 function isCollide(sarr){
+
+    //highscore
+
+    if (score>highscore){
+        highscore = score;
+    }
+    //bumping into urself
+
+    for (let i = 1; i < snakeArr.length; i++) {
+
+        if (snakeArr[0].x === snakeArr[i].x && snakeArr[0].y === snakeArr[i].y){
+            return true;
+        }
+
+        time++;
+        
+    }
+
+    //bumping into the wall
+
+    if ((snakeArr[0].x >= 18 || snakeArr[0].x <=0) || (snakeArr[0].y >= 18 || snakeArr[0].y <=0 )){
+        return true;
+    }
+
+    
     return false;
 }
 
 function gameEngine(){
+
+    
+
+
     // updating the things
     if (isCollide(snakeArr)){
         inputDir = {x:0, y:0};
-        alert("Game Over. Press any key to play again!");
-        let snakeArr = [{x:13, y:15}];
+        snakeArr = [{x:13, y:15}];
         score = 0;
+        document.getElementById("over").style.opacity = 1;
+        time = 0;
     }
+
+
+
+
 
     // increase the score and respawn the food 
     if (snakeArr[0].y === food.y && snakeArr[0].x === food.x){
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
         let a = 2;
         let b = 16;
+        score++;
+        document.getElementById("score").textContent = score;
         food = {x: Math.round(a +(b-a)*Math.random()), y: Math.round(a +(b-a)*Math.random())}
         
     }
@@ -78,9 +116,16 @@ function gameEngine(){
 
 
 //Main Logic starts here
+document.getElementById("over").style.opacity = 0;
+
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e=>{
+    if (time===0){
+        document.getElementById("score").textContent = score;
+    }
     inputDir = {x:0, y:1} //Start the gmae
+    document.getElementById("over").style.opacity = 0;
+
     switch (e.key) {
         case "ArrowUp":
             inputDir.x = 0;
